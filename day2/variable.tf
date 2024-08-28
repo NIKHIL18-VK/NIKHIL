7b8a281c47666517eb459f7ccc835bff8813b4d0
nikhil18-vk <ragamgarinikhil@gmail.com>
2024-08-28 12:00:29 +0530

    updates

---
+++ day2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    description = "nnn"
+  type = string
+  default = ""
+}
+variable "instance_type" {
+     description = "nnn"
+  type = string
+  default = ""
+  }
+  variable "key_name" {
+     description = "nnn"
+  type = string
+  default = ""
+  }
